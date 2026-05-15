cd504c4ebf3f9ae2bf84e5633d096f6dfff41f03
bahaa24git <ahmad12.alnaggar34@gmail.com>
2026-05-15 17:19:40 +0300

    fixing API requests

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const apiBase = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const API_URL = `${apiBase.replace(/\/+$/, '')}/api`;
 
 const api = axios.create({ baseURL: API_URL });
 
